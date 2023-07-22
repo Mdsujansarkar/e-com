65e4f004ca97f7c43af687af04e240564dc5adb3
Mdsujansarkar <sujansarkar786@gmail.com>
2023-07-22 18:50:11 +0600

    category add

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,10 @@
+@include('admin.common.header')
+    <!-- sidenav  -->
+
+@include('admin.common.sidebar')
+    <!-- end sidenav -->
+
+@include('admin.common.navbar')
+   @yield('main')
+</body>
+@include('admin.common.footer')
